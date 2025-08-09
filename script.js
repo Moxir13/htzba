f151ec52c3b43b712e31544b98c90487cb94e6ff
Moxir13 <r9294982@gmail.com>
2025-08-09 10:05:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let a = prompt("String typeda ma'lumot kiriting:");
+let b = +prompt("Number typeda ma'lumot kiriting:");
+let c = prompt("Bolean typeda ma'lumot kiriting:");
+
+confirm("kiritilgan ma'lumotlar to'g'rimi?:");
+ 
